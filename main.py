79d18463af75700fae82c83cff22d3f15c5b154c
jpbelval <jp@belval.org>
2025-02-03 22:57:18 -0500

    Init Chapter 2, Section 7

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+from importlib.metadata import version
+from dataset import GPTDatasetV1, create_dataloader_v1
+
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
